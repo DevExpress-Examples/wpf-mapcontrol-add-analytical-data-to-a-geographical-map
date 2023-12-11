aa4d60d565043d991ad20c34b0b8aa2b58605c70
DevExpressExampleBot <fakeEmail>
2023-12-11 12:08:35 +0400

    Source auto update [skip ci]

---
+++ VB/Wpf_MapControl_Lesson3/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace Wpf_MapControl_Lesson3
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
